28e012b442c29c1dd21b627b29090affa34d79b0
Julia Fong <Juliamzfong@gmail.com>
2020-03-18 11:22:38 -0500

    Adds section 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3{
+    color: blue;
+  }
+  `]
 })
 export class AppComponent {
   name = 'jules';
